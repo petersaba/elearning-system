76db60cda9da83fafc936c92327d39c78b5eb509
Peter Saba <petersaba001@gmail.com>
2022-10-08 17:50:18 +0300

    frontend <> login: styling hero section and paragraph

--- frontend/src/components/LoginSignUpSwitch.js
+++ frontend/src/components/LoginSignUpSwitch.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const LoginSignUpSwitch = ({ type }) => {
     return (
-        <p>{type == "login" ? "Don't " : ""}already have an account?
+        <p className="switch-pages">{type == "login" ? "Don't " : ""}already have an account?
         <Link to={type == "login" ? "/signup" : "/"}>{type == "login" ? "Sign Up" : "Login"}</Link></p>
     )
 }
