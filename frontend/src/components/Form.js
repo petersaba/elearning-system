5f9bb82b17cdf16fd62fe75aeef57fb0a44e96e1
Peter Saba <petersaba001@gmail.com>
2022-10-09 01:35:46 +0300

    frontend <> login: retrieving input data

--- frontend/src/components/Form.js
+++ frontend/src/components/Form.js
@@ -1,17 +1,48 @@
+import { useEffect, useState } from "react";
 import Button from "./Button";
 import Input from "./Input";
 import LoginSignUpSwitch from "./LoginSignUpSwitch";
 
 const Form = ({ type, onClick }) => {
-    if(type == 'login'){
+    const [inputValues, setInputValues] = useState({});
+
+    console.log("executed");
+
+    useEffect(() => {
+        console.log(inputValues);
+    }, [inputValues]);
+
+    function setParentValue(attribute, value){
+        setInputValues({...inputValues, [attribute]: value});
+    }
+
+    // Form for the login
+    if(type == 'Login'){
         return (
             <form className="form">
                 <h1>{type}</h1>
-                <Input type={'text'} id="email" text={'Email'}/>
-                <Input type={'password'} id="password" text={'Password'}/>
+                <Input type={'text'} id="email" text={'Email'} onChange={setParentValue}/>
+                <Input type={'password'} id="password" text={'Password'} onChange={setParentValue}/>
                 <div>
-                    <Button text='Login'></Button>
+                    <Button text={type}></Button>
                     <LoginSignUpSwitch/>
+                </div>
+            </form>
+        );
+    }
+
+    // form for the sign up
+    if(type == 'Sign Up'){
+        return (
+            <form className="form">
+                <h1>{type}</h1>
+                <Input type={'text'} id="email" text={'Email'} />
+                <Input type={'text'} id="full_name" text={'Full Name'} onChange={setParentValue}/>
+                <Input type={'password'} id="password" text={'Password'} onChange={setParentValue}/>
+                <Input type={'password'} id="confirm_password" text={'Confirm Password'} onChange={setParentValue}/>
+                <div>
+                    <Button text={type}></Button>
+                    <LoginSignUpSwitch type={'Sign Up'}/>
                 </div>
             </form>
         );
@@ -19,7 +50,7 @@ const Form = ({ type, onClick }) => {
 }
 
 Form.defaultProps = {
-    type: 'login'
+    type: 'Login'
 }
 
 export default Form;
--- frontend/src/components/Input.js
+++ frontend/src/components/Input.js
@@ -1,7 +1,9 @@
-const Input = ({ id, type, text }) => {
+import onChange from "../utilities";
+
+const Input = ({ id, type, text, onChange }) => {
     return (
         <div className="input">
-            <input id={id} type={type} placeholder="this will not show"/>
+            <input id={id} type={type} placeholder="this will not show" onChange={(e) => onChange(id, e.target.value)}/>
             <label htmlFor={id}>{text}</label>
         </div>
     );
