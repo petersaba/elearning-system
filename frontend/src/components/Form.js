612aee540f4b51b1ac930489d45e5acd8864c4a5
Peter Saba <petersaba001@gmail.com>
2022-10-08 13:46:13 +0300

    frontend <> login: adding input components

--- frontend/src/components/Form.js
+++ frontend/src/components/Form.js
@@ -1,8 +1,12 @@
+import Input from "./Input";
+
 const Form = ({ type, onClick }) => {
     if(type == 'login'){
         return (
             <form className="form">
                 <h1>{type}</h1>
+                <Input type={'text'} id="email" text={'Email'}/>
+                <Input type={'password'} id="password" text={'Password'}/>
             </form>
         );
     }
---
+++ frontend/src/components/Input.js
@@ -0,0 +1,10 @@
+const Input = ({ id, type, text }) => {
+    return (
+        <div className="input">
+            <input id={id} type={type} placeholder="this will not show"/>
+            <label for={id}>{text}</label>
+        </div>
+    );
+}
+
+export default Input;
