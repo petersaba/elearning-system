a5e70f4caefc855545ab32014d5d0f366e8aba4b
Peter Saba <petersaba001@gmail.com>
2022-10-09 14:36:12 +0300

    frontend <> login: resetting input value when changing path

--- frontend/src/components/Input.js
+++ frontend/src/components/Input.js
@@ -1,9 +1,22 @@
-import onChange from "../utilities";
+import { useEffect, useState } from "react";
 
 const Input = ({ id, type, text, onChange }) => {
+    const [path, setPath] = useState('/');
+    const [value, SetValue] = useState('');
+
+    if(path != window.location.pathname){
+        setPath(window.location.pathname);
+    }
+
+    useEffect(() => {
+        console.log(path);
+        SetValue('');
+    }, [path]);
+    
     return (
-        <div className="input">
-            <input id={id} type={type} placeholder="this will not show" onChange={(e) => onChange(id, e.target.value)}/>
+        <div className="input"> 
+            <input id={id} type={type} placeholder="this will not show" onChange={(e) => {onChange(id, e.target.value);
+                                                                                            SetValue(e.target.value);}} value={value}/>
             <label htmlFor={id}>{text}</label>
         </div>
     );
