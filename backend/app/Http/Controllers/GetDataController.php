1b1607d40b90341558763a3878fe0cf2a5f9f882
Peter Saba <petersaba001@gmail.com>
2022-10-10 21:34:54 +0300

    backend <> get users: validating data sent

--- backend/app/Http/Controllers/GetDataController.php
+++ backend/app/Http/Controllers/GetDataController.php
@@ -8,6 +8,13 @@ use Illuminate\Http\Request;
 class GetDataController extends Controller
 {
     function getUsersByType($type){
+        if($type != 'student' && $type != 'instructor'){
+            return response()->json([
+                'status' => 'Error',
+                'message' => 'Data is invalid'
+            ], 415);
+        }
+
         $users = User::where('type', $type)->get();
         return response()->json([
             'status' => 'Success',
