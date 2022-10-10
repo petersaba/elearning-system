bfad02afc011fcd4f2603f988528db751d756bca
Peter Saba <petersaba001@gmail.com>
2022-10-11 02:56:29 +0300

    frontend <> admin add user: adding layout form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
 import Hero from './components/Hero';
 import Form from './components/Form';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import LoginSignUpSwitch from './components/LoginSignUpSwitch';
+import Dashboard from './components/Dashboard';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <BrowserRouter>
     <section className='body'>
-    <Routes>
-      <Route path='/' element={
-        <>
-          <Hero/>
-          <Form/>
-        </>
-      }/>
-      <Route path='/signup' element={
-        <>
-          <Hero/>
-          <LoginSignUpSwitch type={'signup'}/>
-        </>
-      }/>
-    </Routes>
+      <Routes>
+        <Route path='/' element={
+          <>
+            <Hero/>
+            <Form/>
+          </>
+        }/>
+        <Route path='/signup' element={
+          <>
+            <Hero/>
+            <Form type={'Sign Up'}/>
+          </>
+        }/>
+        <Route path='/admin' element={
+          <>
+            <Dashboard type='admin'/>
+            <Navbar/>
+            <Form type='Add User'/>
+          </>
+        }/>
+      </Routes>
     </section>
     </BrowserRouter>
   );
