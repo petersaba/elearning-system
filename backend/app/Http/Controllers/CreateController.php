0fe952833495af8165126e6de791a41600e5ff08
Peter Saba <petersaba001@gmail.com>
2022-10-10 09:47:20 +0300

    backend <> add course: checking if course code already exists

--- backend/app/Http/Controllers/CreateController.php
+++ backend/app/Http/Controllers/CreateController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Course;
 use Illuminate\Http\Request;
 
 class CreateController extends Controller
@@ -18,5 +19,18 @@ class CreateController extends Controller
                 'message' => 'Data is invalid'
             ], 415);
         }
+
+        if($this->isAttributeAlreadyUsed($request->code, 'code')){
+            return response()->json([
+                'status' => 'Error',
+                'message' => 'Course already exists'
+            ], 403);
+        }
+    }
+
+    function isAttributeAlreadyUsed($value, $attribute){
+        $course = Course::where($attribute, $value)->first();
+
+        return $course ? TRUE : FALSE;
     }
 }
