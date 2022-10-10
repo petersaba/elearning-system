3448652c9a182d37128e02c0954dc693c2df4906
Peter Saba <petersaba001@gmail.com>
2022-10-11 01:11:48 +0300

    frontend <> dashboard: adding and styling links in dashboard

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,12 +1,14 @@
+import { Link } from "react-router-dom";
+
 const Dashboard = ({ type, full_name }) => {
     if(type == 'admin'){
         return (
             <div className="dashboard">
                 <h3>Admin full name</h3>
                 <ul>
-                    <li>Add User</li>
-                    <li>Add Course</li>
-                    <li>Assign Instructor</li>
+                    <li><Link to='/admin'>Add User</Link></li>
+                    <li><Link to='/admin'>Add Course</Link></li>
+                    <li><Link to='/admin'>Assign Instructor</Link></li>
                 </ul>
             </div>
         );
