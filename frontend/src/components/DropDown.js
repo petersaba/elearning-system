9895fa63f336f962295e374af5cd292b0683e448
Peter Saba <petersaba001@gmail.com>
2022-10-11 07:48:52 +0300

    frontend <> assign instructor: adding dropdown menu component for users and courses

--- frontend/src/components/DropDown.js
+++ frontend/src/components/DropDown.js
@@ -1,14 +1,27 @@
 const DropDown = ( props ) => {
-    return (
-        <select className="select-input" onChange={(e) => {props.onChange('type', e.target.value)}}>
+    if(props.type == 'user_type'){
+        return (
+            <select className="select-input" onChange={(e) => {props.onChange('type', e.target.value)}}>
             {Object.keys(props).map((value) => {
-                if(value != 'onChange'){
+                if(value != 'onChange' || value != 'type'){
                     return (
                         <option value={props[value]}>{props[value]}</option>
                         );
                     }
-                })
-            }
+                })}
+            </select>
+        );
+    }
+
+    return (
+        <select className="select-input" onChange={(e) => {props.onChange(props.type, e.target.value)}}>
+        {Object.keys(props).map((value) => {
+            if(value != 'onChange' || value != 'type'){
+                return (
+                    <option value={props[value].id}>{props.type == 'user' ? props[value].full_name : props.name}</option>
+                    );
+                }
+            })}
         </select>
     )
 }
